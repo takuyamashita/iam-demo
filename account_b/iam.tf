92492926658f44af5275e39fa3d7e86bdd548b5e
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-04-23 23:31:44 +0900

    add policy

--- account_b/iam.tf
+++ account_b/iam.tf
@@ -7,7 +7,7 @@ resource "aws_iam_role" "developer" {
 }
 
 ##################
-# iam assume policy
+# iam trust policy
 ##################
 data "aws_iam_policy_document" "developer_trust_policy" {
   statement {
@@ -22,4 +22,30 @@ data "aws_iam_policy_document" "developer_trust_policy" {
 resource "aws_iam_role_policy_attachment" "ec2_full" {
   role       = aws_iam_role.developer.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+##################
+# iam role policy
+##################
+resource "aws_iam_role_policy_attachment" "create_lambda" {
+  role       = aws_iam_role.developer.name
+  policy_arn = aws_iam_policy.create_lambda.arn
+}
+
+resource "aws_iam_policy" "create_lambda" {
+  name   = "create-lambda"
+  policy = data.aws_iam_policy_document.create_lambda.json
+}
+
+data "aws_iam_policy_document" "create_lambda" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:CreateRole",
+      "iam:DeleteRole",
+      "iam:AttachRolePolicy",
+      "lambda:CreateFunction"
+    ]
+    resources = ["*"]
+  }
 }
